fa058cc40e4cda9128f815af578ae2122bd745c8
tahara <tahara@userlocal.jp>
2019-05-23 11:10:29 +0900

    add: a

--- readline.rb
+++ readline.rb
@@ -1,24 +1,26 @@
-require 'formula'
-
 class Readline < Formula
-  homepage 'http://tiswww.case.edu/php/chet/readline/rltop.html'
-  url 'http://ftpmirror.gnu.org/readline/readline-6.3.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/readline/readline-6.3.tar.gz'
-  sha256 '56ba6071b9462f980c5a72ab0023893b65ba6debb4eeb475d7a563dc65cafd43'
-  version '6.3.5'
+  desc "Library for command-line editing"
+  homepage "https://tiswww.case.edu/php/chet/readline/rltop.html"
+  url "https://ftpmirror.gnu.org/readline/readline-6.3.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/readline/readline-6.3.tar.gz"
+  version "6.3.8"
+  sha256 "56ba6071b9462f980c5a72ab0023893b65ba6debb4eeb475d7a563dc65cafd43"
 
   bottle do
     cellar :any
-    sha1 "f18f34972c5164ea4cb94b3311e52fc04ea4b9a9" => :mavericks
-    sha1 "131d59e8bb99e5a9d0270a04e63c07d794750695" => :mountain_lion
-    sha1 "b119b5a05f21f9818b6c99e173597fba62d89b58" => :lion
+    sha256 "11f2ad09f5eb9e055b2567c4308f35cc8e006b9ffdfe49599e4c4d92a0a3a066" => :sierra
+    sha256 "9d38481c935cef21ead25c294285b78a9e8fa556fd15ede9126926c055c40d37" => :el_capitan
+    sha256 "d0cdb3e162c05e21c7184e86629f04685d4be98e38cbf024373142bf8764500e" => :yosemite
+    sha256 "adcc6352d3fb271a1e9ce034d80996405a5c46afbd1bab1507d3ff5e89e02bc1" => :mavericks
+    sha256 "6b1d7e806e169e2b778371d74e7cc62106c348ea9f5c80fd658b763b94dc748e" => :mountain_lion
+    sha256 "c129333634dd00ab2267ae9c531fca1f5cc50dd519ed3399918289fdfdf2663b" => :lion
   end
 
-  keg_only <<-EOS
-OS X provides the BSD libedit library, which shadows libreadline.
-In order to prevent conflicts when programs look for libreadline we are
-defaulting this GNU Readline installation to keg-only.
-EOS
+  keg_only :shadowed_by_osx, <<-EOS.undent
+    OS X provides the BSD libedit library, which shadows libreadline.
+    In order to prevent conflicts when programs look for libreadline we are
+    defaulting this GNU Readline installation to keg-only.
+  EOS
 
   # Vendor the patches.
   # The mirrors are unreliable for getting the patches, and the more patches
@@ -26,15 +28,10 @@ EOS
   # reduce bug reports.
   # Upstream patches can be found in:
   # http://git.savannah.gnu.org/cgit/readline.git
-#  patch do
-#    url "https://gist.githubusercontent.com/jacknagel/8df5735ae9273bf5ebb2/raw/827805aa2927211e7c3d9bb871e75843da686671/readline.diff"
-#    sha1 "2d55658a2f01fa14a029b16fea29d20ce7d03b78"
-#  end
-
   def install
     ENV.universal_binary
     system "./configure", "--prefix=#{prefix}", "--enable-multibyte"
-    system "make install"
+    system "make", "install"
 
     # The 6.3 release notes say:
     #   When creating shared libraries on Mac OS X, the pathname written into the
@@ -44,5 +41,22 @@ EOS
     # This should be removed at 6.4.
     lib.install_symlink "libhistory.6.3.dylib" => "libhistory.6.2.dylib",
                         "libreadline.6.3.dylib" => "libreadline.6.2.dylib"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <stdio.h>
+      #include <stdlib.h>
+      #include <readline/readline.h>
+
+      int main()
+      {
+        printf("%s\\n", readline("test> "));
+        return 0;
+      }
+    EOS
+    system ENV.cc, "-L", lib, "test.c", "-lreadline", "-o", "test"
+    assert_equal "test> Hello, World!\nHello, World!",
+      pipe_output("./test", "Hello, World!\n").strip
   end
 end
